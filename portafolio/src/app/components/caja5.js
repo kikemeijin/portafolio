8df2f89da1d538dc61cbb394642f6c35d5e464cc
kikemeijin <58044374+kikemeijin@users.noreply.github.com>
2024-01-27 15:11:52 +0000

    mensaje

--- portafolio/src/app/components/caja5.js
+++ portafolio/src/app/components/caja5.js
@@ -12,7 +12,82 @@ function Caja5(){
                         <p className={style["detail"]}>ESTE ES UNA SIMULACION DE UN REPOSITORIO</p>
                         <button className={style["btn"]} href="https://github.com" target="_blank">
                             
-                                <img src="github.svg" alt="link"/>
+                                <img src="github.svg" alt="link" />
+                            
+                        </button>
+                    </div>
+                </div>
+                <div className={style["content-card-img"]}>
+                    <img className={style["image"]} src="alpha.jpg" />
+                    <div className={style["content-card-img-overlay"]}>
+
+                    </div>
+                    <div className={style["content-card-img-detail"]}>
+                        <p className={style["title"]}>repositorio 1</p>
+                        <p className={style["detail"]}>ESTE ES UNA SIMULACION DE UN REPOSITORIO</p>
+                        <button className={style["btn"]} href="https://github.com" target="_blank">
+                            
+                                <img src="github.svg" alt="link" />
+                            
+                        </button>
+                    </div>
+                </div>
+                <div className={style["content-card-img"]}>
+                    <img className={style["image"]} src="alpha.jpg" />
+                    <div className={style["content-card-img-overlay"]}>
+
+                    </div>
+                    <div className={style["content-card-img-detail"]}>
+                        <p className={style["title"]}>repositorio 1</p>
+                        <p className={style["detail"]}>ESTE ES UNA SIMULACION DE UN REPOSITORIO</p>
+                        <button className={style["btn"]} href="https://github.com" target="_blank">
+                            
+                                <img src="github.svg" alt="link" />
+                            
+                        </button>
+                    </div>
+                </div>
+                <div className={style["content-card-img"]}>
+                    <img className={style["image"]} src="alpha.jpg" />
+                    <div className={style["content-card-img-overlay"]}>
+
+                    </div>
+                    <div className={style["content-card-img-detail"]}>
+                        <p className={style["title"]}>repositorio 1</p>
+                        <p className={style["detail"]}>ESTE ES UNA SIMULACION DE UN REPOSITORIO</p>
+                        <button className={style["btn"]} href="https://github.com" target="_blank">
+                            
+                                <img src="github.svg" alt="link" />
+                            
+                        </button>
+                    </div>
+                </div>
+                <div className={style["content-card-img"]}>
+                    <img className={style["image"]} src="alpha.jpg" />
+                    <div className={style["content-card-img-overlay"]}>
+
+                    </div>
+                    <div className={style["content-card-img-detail"]}>
+                        <p className={style["title"]}>repositorio 1</p>
+                        <p className={style["detail"]}>ESTE ES UNA SIMULACION DE UN REPOSITORIO</p>
+                        <button className={style["btn"]} href="https://github.com" target="_blank">
+                            
+                                <img src="github.svg" alt="link" />
+                            
+                        </button>
+                    </div>
+                </div>
+                <div className={style["content-card-img"]}>
+                    <img className={style["image"]} src="alpha.jpg" />
+                    <div className={style["content-card-img-overlay"]}>
+
+                    </div>
+                    <div className={style["content-card-img-detail"]}>
+                        <p className={style["title"]}>repositorio 1</p>
+                        <p className={style["detail"]}>ESTE ES UNA SIMULACION DE UN REPOSITORIO</p>
+                        <button className={style["btn"]} href="https://github.com" target="_blank">
+                            
+                                <img src="github.svg" alt="link" />
                             
                         </button>
                     </div>
